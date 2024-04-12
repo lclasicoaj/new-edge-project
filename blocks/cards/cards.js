7143b356d5f1dcea6a5ecb50bf0cd7c385611b53
ajay <achavan@adobe.com>
2024-04-12 12:17:36 +0530

    commit to card

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,6 +3,7 @@ import { createOptimizedPicture } from '../../scripts/aem.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
+  
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
@@ -15,4 +16,5 @@ export default function decorate(block) {
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
+
 }
